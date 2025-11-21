24e0c4da149ab132a8622fde14d8c7c95ebf705e
humajko <matus.humaj@gmail.com>
2025-11-21 17:32:17 +0100

    Generovane cez Fiori generator

---
+++ mypage/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("mypage.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ mypage/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mypage/test/unit/controller/MainPage.controller"
+], function () {
+	"use strict";
+});
