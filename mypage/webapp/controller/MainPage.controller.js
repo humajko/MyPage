3d816cf0127382fca411021fc368346669234a7e
humajko <matus.humaj@gmail.com>
2025-11-21 21:10:51 +0100

    Main

--- mypage/webapp/controller/MainPage.controller.js
+++ mypage/webapp/controller/MainPage.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("mypage.controller.MainPage", {
         onInit() {
+            var oJsonModel = new JSONModel({
+				AvatarPicture: sap.ui.require.toUrl("images/ja_avatar.jpg")
+			});
+			this.getView().setModel(oJsonModel, "Pictures");
         }
     });
 });
